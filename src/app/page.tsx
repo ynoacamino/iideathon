03bbe9bb22a237aa06947b9c95b16dc8a8be842f
gustadev24 <lgsc21211@gmail.com>
2025-05-31 13:50:44 -0500

    feat(component): create triangles grid component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,10 @@
-import { StarGrid } from "@/components/ui/StarGrid";
+import { Triangles } from "@/components/ui/triangles";
 
 export default function Home() {
   return (
     <div className="">
       <main className="flex flex-col">
-        <StarGrid size="lg" rows={4} cols={4} />
+      <Triangles rows={3} cols={3} size="md"/>
       </main>
     </div>
   );
---
+++ src/components/ui/stars.tsx
@@ -0,0 +1,54 @@
+import Star from '@/assets/star.svg';
+import { cn } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
+
+const starVariants = cva(
+  '',
+  {
+    variants: {
+      size: {
+        sm: 'size-4',
+        md: 'size-6',
+        lg: 'size-8',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    }
+  }
+);
+
+type StarGridProps =  {
+  rows?: number;
+  cols?: number;
+  className?: string;
+} & VariantProps<typeof starVariants>;
+
+function Stars({
+  rows = 1, cols = 1, size, className,
+}: StarGridProps) {
+  return (
+    <div
+      className={cn(
+        "grid gap-1 w-max",
+        className,
+      )}
+      style={{
+        gridTemplateRows: `repeat(${rows}, 1fr)`,
+        gridTemplateColumns: `repeat(${cols}, 1fr)`,
+      }}
+    >
+      {
+        Array.from({ length: rows * cols }).map((_, i) => (
+          <Star
+            key={`star-${i}-${Math.random()}`} // TODO: It is better creating a context that keeps stars amount on the page
+            className={starVariants({ size })}
+            aria-label="Star"
+          />
+        ))
+      }
+    </div>
+  );
+}
+
+export { Stars };
