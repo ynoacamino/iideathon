f389d082066c92862dc25fd5a3fb8083fe1e3591
gustadev24 <lgsc21211@gmail.com>
2025-05-31 13:13:13 -0500

    feat(star-ui): add size for each star element in stars grid

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,8 +3,8 @@ import { StarGrid } from "@/components/ui/StarGrid";
 export default function Home() {
   return (
     <div className="">
-      <main className="">
-        <StarGrid />
+      <main className="flex flex-col">
+        <StarGrid size="lg" rows={4} cols={4} />
       </main>
     </div>
   );
--- src/components/ui/StarGrid.tsx
+++ src/components/ui/StarGrid.tsx
@@ -1,16 +1,38 @@
 import Star from '@/assets/star.svg';
+import { cn } from '@/lib/utils';
+import { cva, VariantProps } from 'class-variance-authority';
 
 interface StarGridProps {
   rows?: number;
   cols?: number;
+  className?: string;
 }
 
+const starVariants = cva(
+  '',
+  {
+    variants: {
+      size: {
+        sm: 'size-4',
+        md: 'size-6',
+        lg: 'size-8',
+      },
+    },
+    defaultVariants: {
+      size: 'md',
+    }
+  }
+);
+
 function StarGrid({
-  rows = 1, cols = 1,
-}: StarGridProps) {
+  rows = 1, cols = 1, size, className,
+}: StarGridProps & VariantProps<typeof starVariants>) {
   return (
     <div
-      className="grid gap-2"
+      className={cn(
+        "grid gap-2 w-max",
+        className,
+      )}
       style={{
         gridTemplateRows: `repeat(${rows}, 1fr)`,
         gridTemplateColumns: `repeat(${cols}, 1fr)`,
@@ -20,7 +42,7 @@ function StarGrid({
         Array.from({ length: rows * cols }).map((_, i) => (
           <Star
             key={`star-${i}-${Math.random()}`}
-            className="w-6 h-6"
+            className={starVariants({ size })}
             aria-label="Star"
           />
         ))
